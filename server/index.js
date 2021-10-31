323f0f228d9721da56fe87c3a262748d68c7eb9a
TrungTN <tangoctrung12123@gmail.com>
2021-10-31 19:05:07 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -24,11 +24,6 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-app.use(express.static(path.join(__dirname, "/client")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-})
 
 // Socket
 const httpServer = require('http').createServer(app);
